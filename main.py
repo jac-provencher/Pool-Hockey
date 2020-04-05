288cec9082ac58b11329c4199dd93bf8e073545a
JacobProvencher <jacob.provencher.1@ulaval.ca>
2020-04-04 20:51:45 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+class Circle:
+
+    def __init__(self):
+        pass
+
+class Player:
+
+    def __init__(self):
+        pass
+
+class Surface:
+
+    def __init__(self):
+        pass
+
+class Game:
+
+    def __init__(self):
+
+        self.surface = Surface()
+        self.player1 = Player()
+        self.player2 = Player()
