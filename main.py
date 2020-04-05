2e14787d849ce3bf89d835395d99f21263168d26
JacobProvencher <jacob.provencher.1@ulaval.ca>
2020-04-04 21:05:04 -0400

    second push

--- main.py
+++ main.py
@@ -4,14 +4,34 @@ class Circle:
     def __init__(self):
         pass
 
+    def move(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
 class Player:
 
     def __init__(self):
         pass
 
+    def move(self):
+        pass
+
+class Puck:
+
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
 class Surface:
 
     def __init__(self):
+        pass
+
+    def draw(self):
         pass
 
 class Game:
@@ -21,3 +41,10 @@ class Game:
         self.surface = Surface()
         self.player1 = Player()
         self.player2 = Player()
+        self.puck = Puck()
+
+    def redraw(self):
+        pass
+
+    def mainloop(self):
+        pass
